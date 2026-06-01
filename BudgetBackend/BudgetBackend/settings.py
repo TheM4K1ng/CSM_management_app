4a583c4551f4924df2d49535eee982cb22a581cd
TheM4K1ng <oribenyshay@gmail.com>
2026-06-01 09:50:45 +0300

    fixed back and front

--- BudgetBackend/BudgetBackend/settings.py
+++ BudgetBackend/BudgetBackend/settings.py
@@ -17,6 +17,10 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 load_dotenv(BASE_DIR / ".env")
 
 
